b62c20a06edfcc5c812a4d3987b8f724b7b422aa
DmitriyKanischev <116942865+DmitriyKanischev@users.noreply.github.com>
2023-06-18 15:17:24 +0400

    app use cors added

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('../schema/schema');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3005;
@@ -12,6 +13,8 @@ app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
+
+app.use(cors())
 
 const dbConnection = mongoose.connection;
 dbConnection.on('error', err => console.log(`Connection error ${err}`));
